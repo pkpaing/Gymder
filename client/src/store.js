4eb0f718cf21ddc301e7857cc4db81c6f830cc6c
Oliver Loo <47163438+Oliverloo0909@users.noreply.github.com>
2022-05-30 15:25:16 +0800

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -5,10 +5,4 @@ import rootReducer from "./reducers";
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
-);
-
 export default store;
