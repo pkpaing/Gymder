4cd92a38747e75273f48d3fe35753dbe90c74157
pkpaing <pkpaing@gmail.com>
2022-06-23 19:35:37 +0800

    More Info Popups

--- client/src/components/MoreInfo.js
+++ client/src/components/MoreInfo.js
@@ -2,18 +2,45 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
-const MoreInfo = ({ setShowInfo }) => {
+const MoreInfo = ({ setShowInfo, cardUser, filteredUsers, setCardUser }) => {
   const handleClick = () => {
     setShowInfo(false);
   };
 
+  if (cardUser === null) {
+    setCardUser(filteredUsers[filteredUsers.length - 1]);
+  }
+
   return (
-    <div className="more-info">
-      <div className="info-close-icon" onClick={handleClick}>
-        ✘
+    cardUser && (
+      <div className="my-profile">
+        <div className="profile-close-icon" onClick={handleClick}>
+          ✘
+        </div>
+        <div className="profile-img-container">
+          <img src={cardUser.url1} alt={"photo of " + cardUser.first_name} />
+        </div>
+        <div className="about">{cardUser.about}</div>
+        <div className="info">
+          Name: {cardUser.first_name}
+          <br />
+          <br />
+          Gender: {cardUser.gender_identity}
+          <br />
+          <br />
+          Age: {cardUser.age}
+          <br />
+          <br />
+          Gym Location: {cardUser.gym_location}
+          <br />
+          <br />
+          Training Type: {cardUser.training_type}
+          <br />
+          <br />
+          Years of Experience: {cardUser.years_experience}
+        </div>
       </div>
-      More Info
-    </div>
+    )
   );
 };
 
